d2b83f306e53fa4c72f68cdd7605de5800fb5de7
cusengan <lordvinh1@live.com>
2017-03-13 13:55:45 -0500

    First commit

---
+++ src/testing/Testing.java
@@ -0,0 +1,20 @@
+
+package testing;
+
+
+public class Testing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Animal Dog = new Dog();
+        Animal Cat = new Cat();
+        
+        Dog.attack();
+        Cat.attack();
+    
+    }
+    
+}
